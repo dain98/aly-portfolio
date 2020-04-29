a89fbccd2de7b8b5dbc936fc0c1376e087941d9c
dain98 <dain98@gmail.com>
2020-04-29 16:13:30 +0900

    dasdlkjfakl;wrgja l;krjg;as

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ class App extends React.Component {
             <div>
                 <NavBar/>
                 <div data-aos={"fade-down"} >
-
                     <FrontPage/>
                 </div>
             </div>
